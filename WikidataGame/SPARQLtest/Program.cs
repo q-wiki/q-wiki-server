b5fd732f4707120ef1e0e43da4cec47e47393f0b
Laura Woelbeling <s0548106@htw-berlin.de>
2019-05-21 13:08:18 +0200

    get rid of unused code and add output for results1
    Note: ORDER BY RAND() seems not to work (Caching problem?); linq shuffling also does not seem to work

--- WikidataGame/SPARQLtest/Program.cs
+++ WikidataGame/SPARQLtest/Program.cs
@@ -2,6 +2,7 @@
 using VDS.RDF.Parsing;
 using VDS.RDF.Query;
 using System.Linq;
+using VDS.RDF;
 
 namespace SPARQLtest
 {
@@ -9,6 +10,10 @@ namespace SPARQLtest
     {
         static void Main(string[] args)
         {
+            // guide: https://github.com/dotnetrdf/dotnetrdf/wiki/UserGuide-Querying-With-SPARQL
+
+
+            Console.WriteLine("Test1");
             //****** 1st try ****************/
             //Create a Parameterized String
             SparqlParameterizedString queryString = new SparqlParameterizedString();
@@ -31,39 +36,40 @@ namespace SPARQLtest
             queryString.Namespaces.AddNamespace("bd", new Uri("http://www.bigdata.com/rdf#"));
 
             //Set the SPARQL command
-            //For more complex queries we can do this in multiple lines by using += on the
-            //CommandText property
+            //For more complex queries we can do this in multiple lines by using += on the CommandText property
             //Note we can use @name style parameters here
-            //queryString.CommandText = "SELECT * WHERE { ?s ex:property @value }";
             queryString.CommandText = "SELECT ?president ?president_name ?signature WHERE { ?president wdt:P39 wd:Q11696. ";
             queryString.CommandText += "?president wdt:P109 ?signature. ";
             queryString.CommandText += "OPTIONAL {?president rdfs:label ?president_name ";
             queryString.CommandText += "filter(lang(?president_name) = 'en').}}";
             queryString.CommandText += " ORDER BY RAND() LIMIT 4";
 
-            //Then we can parse a SPARQL string into a query
-            //SparqlQuery q = parser.ParseFromString("SELECT * WHERE { ?s a ?type }");
-
-            //Inject a Value for the parameter
-            //queryString.SetUri("value", new Uri("http://example.org/value"));
-            queryString.SetUri("president", new Uri("http://query.wikidata.org/value"));
-
             //When we call ToString() we get the full command text with namespaces appended as PREFIX
             //declarations and any parameters replaced with their declared values
             Console.WriteLine(queryString.ToString());
 
             //We can turn this into a query by parsing it as in our previous example
-            //SparqlQueryParser parser = new SparqlQueryParser();
             SparqlQuery query = parser.ParseFromString(queryString);
 
 
-            //Get the Query processor
             //Get the Query processor
             ISparqlQueryProcessor processor = new RemoteQueryProcessor(new SparqlRemoteEndpoint(new Uri("https://query.wikidata.org/bigdata/namespace/wdq/sparql")));
 
             var results = processor.ProcessQuery(query);
 
-             //********* 2nd try **********/
+            if (results is SparqlResultSet)
+            {
+                //Print out the Results
+                SparqlResultSet rset = (SparqlResultSet)results;
+                foreach (SparqlResult result in rset)
+                {
+                    Console.WriteLine(result.ToString());
+                }
+            }
+
+            Console.WriteLine();
+            //********* 2nd try **********/
+            Console.WriteLine("Test2");
 
             SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri(("https://query.wikidata.org/bigdata/namespace/wdq/sparql")));
             SparqlResultSet results2 = endpoint.QueryWithResultSet(queryString.CommandText);

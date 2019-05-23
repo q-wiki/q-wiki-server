1db173943baa27f3d8bfcc160cd1751910d34191
Malte Götz <github@maltegoetz.com>
2019-05-23 18:51:32 +0200

    added xunit test project

---
+++ WikidataGame/WikidataGame.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace WikidataGame.Backend.Tests
+{
+    public class UnitTest1
+    {
+        //Getting started: https://xunit.net/docs/getting-started/netcore/cmdline
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

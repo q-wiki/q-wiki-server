574ab9494e83a14b653826d9cfdac9ba23639a4d
heyarne <arne@schlueter.is>
2019-05-28 12:51:54 +0200

    Add first real tests

---
+++ WikidataGame/WikidataGame.Backend.Tests/MapGeneratorTest.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+using WikidataGame.Backend;
+
+namespace WikidataGame.Backend.Tests
+{
+    public class MapGeneratorTest
+    {
+        //Getting started: https://xunit.net/docs/getting-started/netcore/cmdline
+
+        [Fact]
+        public void MapStructureTest()
+        {
+            var mapCandidate = Services.MapGeneratorService.GenerateMapCandidate(
+                10, 10, 5
+            );
+            Assert.Equal(Enumerable.Count(mapCandidate), 10 * 10);
+            Assert.Equal(Enumerable.Count(mapCandidate.Where(t => t.IsAccessible)), 5);
+        }
+
+        [Fact]
+        public void PlayerJoinTest()
+        {
+            var finalMapCandidate = Services.MapGeneratorService.GenerateMap(10, 10, 5);
+            Assert.All(finalMapCandidate, tile => Assert.Null(tile.Owner));
+            
+            var p1 = new Models.User { DeviceId="device-a" };
+            var p2 = new Models.User { DeviceId="device-b" };
+            var players = new List<Models.User>();
+            players.Add(p1);
+            players.Add(p2);
+            Services.MapGeneratorService.SetStartPositions(finalMapCandidate, players);
+
+            var tileForP1 = finalMapCandidate.Where(t => t.Owner == p1).First();            
+            var tileForP2 = finalMapCandidate.Where(t => t.Owner == p2).First();
+
+            Assert.NotNull(tileForP1);
+            Assert.NotNull(tileForP2);
+            Assert.NotEqual(tileForP1, tileForP2);
+        }
+    }
+}
---
+++ WikidataGame/WikidataGame.Backend.Tests/TileHelperTest.cs
@@ -0,0 +1,20 @@
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Xunit;
+using WikidataGame.Backend;
+
+namespace WikidataGame.Backend.Tests
+{
+    public class TileHelperTest
+    {
+        //Getting started: https://xunit.net/docs/getting-started/netcore/cmdline
+
+        [Fact]
+        public void NeighborTest()
+        {
+          // TODO: Check if neighbors are correctly found for all tiles
+        }
+    }
+}

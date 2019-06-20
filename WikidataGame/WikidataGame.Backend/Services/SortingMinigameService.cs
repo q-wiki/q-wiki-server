b8a1af5ff0e699da608a41e4a5d712c0c11a359d
Malte Götz <github@maltegoetz.com>
2019-06-20 12:58:09 +0200

    added sorting minigame

--- WikidataGame/WikidataGame.Backend/Services/SortingMinigameService.cs
+++ WikidataGame/WikidataGame.Backend/Services/SortingMinigameService.cs
@@ -21,7 +21,20 @@ namespace WikidataGame.Backend.Services
 
         public MiniGame GenerateMiniGame(string gameId, string playerId, string categoryId, string tileId)
         {
-            throw new NotImplementedException();
+            var question = _questionRepo.GetRandomQuestionForMinigameType(MiniGameType, categoryId);
+
+            // use method in baseclass to query wikidata with question
+            var data = QueryWikidata(question.SparqlQuery);
+
+            var minigame = _minigameRepo.CreateMiniGame(gameId, playerId, tileId, categoryId, MiniGameType);
+
+            minigame.TaskDescription = string.Format(question.TaskDescription, data[0].Item1); // placeholder and answer in first tuple!
+            minigame.CorrectAnswer = data.Select(d => d.Item2).ToList(); // placeholder and answer in first tuple!
+            minigame.AnswerOptions = data.Select(item => item.Item2).OrderBy(a => Guid.NewGuid()).ToList(); // shuffle answer options
+
+            _dataContext.SaveChanges();
+
+            return MiniGame.FromModel(minigame);
         }
     }
 }

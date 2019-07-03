c20c98449744d08ed9599991260b181036006492
Malte Götz <github@maltegoetz.com>
2019-07-03 22:58:58 +0200

    fix: incomplete json
    https://stackoverflow.com/questions/54633340/asp-net-core-web-api-2-2-controller-not-returning-complete-json

--- WikidataGame/WikidataGame.Backend/Startup.cs
+++ WikidataGame/WikidataGame.Backend/Startup.cs
@@ -41,7 +41,9 @@ namespace WikidataGame.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(
+                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
 
             services.AddSwaggerGen(c =>
             {

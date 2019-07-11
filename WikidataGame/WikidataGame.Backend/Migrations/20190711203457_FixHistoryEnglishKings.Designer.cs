c7577f867be8db58ae7464a5bef9e4095f82e770
Malte Götz <github@maltegoetz.com>
2019-07-11 22:37:47 +0200

    added migration

--- WikidataGame/WikidataGame.Backend/Migrations/20190711203457_FixHistoryEnglishKings.Designer.cs
+++ WikidataGame/WikidataGame.Backend/Migrations/20190711203457_FixHistoryEnglishKings.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WikidataGame.Backend.Helpers;
 
 namespace WikidataGame.Backend.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190711203457_FixHistoryEnglishKings")]
+    partial class FixHistoryEnglishKings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

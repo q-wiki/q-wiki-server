afa6c92e949a9dbfa8f6569d022a9f0c79334fbe
Laura Woelbeling <s0548106@htw-berlin.de>
2019-07-11 22:13:17 +0200

    remove last migration

--- WikidataGame/WikidataGame.Backend/Migrations/DataContextModelSnapshot.cs
+++ WikidataGame/WikidataGame.Backend/Migrations/DataContextModelSnapshot.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WikidataGame.Backend.Helpers;
 
@@ -14,7 +15,9 @@ namespace WikidataGame.Backend.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+                .HasAnnotation("ProductVersion", "2.2.4-servicing-10062")
+                .HasAnnotation("Relational:MaxIdentifierLength", 128)
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
             modelBuilder.Entity("WikidataGame.Backend.Models.Category", b =>
                 {
@@ -729,29 +732,27 @@ namespace WikidataGame.Backend.Migrations
                             CategoryId = "f9c52d1a-9315-423d-a818-94c1769fffe5",
                             MiniGameType = 0,
                             SparqlQuery = @"#English kings until 1707
-                        SELECT DISTINCT ?question ?answer ?reignstart ?reignend WHERE {
-                          {
-                            SELECT DISTINCT ?human ?name (MIN(?reignstart) as ?reignstart) (MIN(?reignend) as ?reignend) WHERE {
-                              ?human wdt:P31 wd:Q5;
-                                p:P39 ?memberOfStatement.
-                              ?memberOfStatement rdf:type wikibase:BestRank;
-                                ps:P39 wd:Q18810062;
-                                pq:P580 ?reignstart;
-                                pq:P582 ?reignend.
-                              FILTER(?reignstart >= '1066-12-31T00:00:00Z'^^xsd:dateTime)
-                              MINUS { ?human wdt:P97 wd:Q719039. }
-                              SERVICE wikibase:label {
-                                bd:serviceParam wikibase:language 'en'.
-                                ?human rdfs:label ?name.
-                              }
-                            } GROUP BY ?human ?name
-                            ORDER BY (MD5(CONCAT(STR(?name), STR(NOW()))))
-                            LIMIT 4
-                          }
-                          BIND(?name AS ?answer)
-                          BIND('the beginning of their reigning period' AS ?question)
-                        }
-                        ORDER BY (?reignstart)",
+                        SELECT DISTINCT ?question ?answer WHERE {
+                          {SELECT DISTINCT ?human ?name ?reignstart ?reignend WHERE {
+                            ?human wdt:P31 wd:Q5.      #find humans
+                            ?human p:P39 ?memberOfStatement.
+                            ?memberOfStatement a wikibase:BestRank;
+                                                 ps:P39 wd:Q18810062. # position
+
+                            ?memberOfStatement pq:P580 ?reignstart;
+                                               pq:P582 ?reignend. 
+                            FILTER (?reignstart >= '1066-12-31T00:00:00Z'^^xsd:dateTime) . #start with William the Conquerer
+                            MINUS {?human wdt:P97 wd:Q719039.}
+
+                            SERVICE wikibase:label {
+                              bd:serviceParam wikibase:language 'en'.
+                              ?human  rdfs:label ?name.
+                            }
+                          } ORDER BY MD5(CONCAT(STR(?name), STR(NOW())))
+                          LIMIT 4}
+                                BIND (?name as ?answer).
+                                BIND ('the beginning of their reigning period' as ?question).
+                        } ORDER BY ?reignstart",
                             TaskDescription = "Sort these English kings by {0} (ascending)."
                         },
                         new

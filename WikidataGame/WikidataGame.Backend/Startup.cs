f91fa63c1509c883eea7c402b5944aa3dc6a5816
Malte Götz <github@maltegoetz.com>
2020-01-13 21:35:17 +0100

    enabled cors for platform endpoints

--- WikidataGame/WikidataGame.Backend/Startup.cs
+++ WikidataGame/WikidataGame.Backend/Startup.cs
@@ -44,6 +44,11 @@ namespace WikidataGame.Backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAny", builder => builder.AllowAnyOrigin());
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(
                 options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );

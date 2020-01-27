2f0f7e54d7d39682a2525b6a16aef5160a541aa7
Eko <e.supardjo@gmx.de>
2020-01-27 21:50:28 +0100

    fixed queries: "Sort actors by movie awards received." and "Sort sports by participating players"
    * "Sort actors by movie awards received": number of awards was counted wrong
    * "Sort sports by maximum amount participating of players": Clarified taskdescription, so user knows it is about maximum amount of participatiing players.

--- WikidataGame/WikidataGame.Backend/Helpers/DatabaseSeeds.cs
+++ WikidataGame/WikidataGame.Backend/Helpers/DatabaseSeeds.cs
@@ -3725,61 +3725,65 @@ namespace WikidataGame.Backend.Helpers
                        TaskDescription = "Sort these actors by the number of movie awards they have received.",
                        SparqlQuery = @"
                             SELECT DISTINCT ?question ?answer ?awardCount
-                                WITH{
-                                                               # select the 500 movies with the highest box office revenue
-                                SELECT DISTINCT ?movie WHERE {
-                                  ?movie wdt:P31 wd:Q11424.
-                                  ?movie p:P2142/psv:P2142 [wikibase:quantityAmount ?boxOffice; wikibase:quantityUnit ?currency].
-                                  # only look at us dollars
-                                  FILTER(?currency = wd:Q4917)
-                                }
-                                ORDER BY DESC(?boxOffice)
-                                LIMIT 500
-                                } as %topGrossingMovies
+                            WITH{
+                                                           # select the 500 movies with the highest box office revenue
+                            SELECT DISTINCT ?movie WHERE {
+                              ?movie wdt:P31 wd:Q11424.
+                              ?movie p:P2142/psv:P2142 [wikibase:quantityAmount ?boxOffice; wikibase:quantityUnit ?currency].
+                              # only look at us dollars
+                              FILTER(?currency = wd:Q4917)
+                            }
+                            ORDER BY DESC(?boxOffice)
+                            LIMIT 500
+                            } as %topGrossingMovies
 
-                                WITH{
-                                SELECT DISTINCT ?actor ?award
-                                WHERE{
-                                   {INCLUDE %topGrossingMovies}
-                                  # get all actors that played in those movies
-                                  ?movie wdt:P161 ?actor.
-                                  ?actor wdt:P166 ?award.
-                                  }
-                                  ORDER BY MD5(CONCAT(STR(?actor), STR(NOW())))
-                                  limit 500
-                                } as %allWinners
 
-                                WITH{
-                                SELECT DISTINCT ?actorLabel (count(?award) as ?awardCount)
-                                  WHERE {
-                                         {INCLUDE %allWinners.}
-                                         ?award wdt:P31*/wdt:P279* wd:Q4220917
-                                         #?baseAward. logic if we want to count movie and television awards
-                                         #VALUES ?baseAward {wd:Q4220917 wd:Q1407225}
-                                         SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. 
-                                                                                         ?actor rdfs:label ?actorLabel
-                                                                                        }
-                                  }
-                                  group by ?actorLabel
-                                } as %countedMovies
+                            WITH{
+                            SELECT DISTINCT ?actor
+                            WHERE{
+                               {INCLUDE %topGrossingMovies}
+                              # get all actors that played in those movies
+                              ?movie wdt:P161 ?actor.
+                              ?actor wdt:P166 ?award.
+                              }
+                              ORDER BY MD5(CONCAT(STR(?award), STR(NOW())))
+                              limit 150
+                            } as %allWinners
 
-                                WITH{
-                                   select (Group_Concat(distinct sample(?actorLabel); separator=', ') as ?actors) ?awardCount
-                                   where {
-                                        include %countedMovies
-                                        filter(?awardCount > 1)
-                                   }
-                                   group by ?awardCount
-                                   ORDER BY MD5(CONCAT(STR(?actor), STR(NOW())))
-                                   limit 4
-                                } as %summedActors
+                            WITH{
+                            SELECT DISTINCT ?actorLabel (count(?award) as ?awardCount)
+                              WHERE {
+                                     {
+                                        SELECT ?actor
+                                        WHERE {include %allWinners}
+                                     }
+                                     ?actor wdt:P166 ?award.
+                                     ?award wdt:P31*/wdt:P279* wd:Q4220917
+                                     #?baseAward.
+                                     #VALUES ?baseAward {wd:Q4220917 wd:Q1407225}
+                                     SERVICE wikibase:label { bd:serviceParam wikibase:language 'en'. 
+                                                                                     ?actor rdfs:label ?actorLabel
+                                                                                    }
+                              }
+                              group by ?actorLabel
+                            } as %countedMovies
 
-                                WHERE{
-                                    {INCLUDE %summedActors}
-                                    BIND(?actors as ?answer)
-                                    BIND('Sort actors by oscars received.' as ?question)
-                                }
-                                ORDER BY asc(?awardCount)
+                            WITH{
+                               select (Group_Concat(distinct  sample(?actorLabel); separator=', ') as ?actors) ?awardCount
+                               where {
+                                    include %countedMovies
+                               }
+                               group by ?awardCount
+                               ORDER BY MD5(CONCAT(STR(?actor), STR(NOW())))
+                               limit 4
+                            } as %summedActors
+
+                            WHERE{
+                                {INCLUDE %summedActors}
+                                BIND(?actors as ?answer)
+                                BIND('Sort actors by oscars received.' as ?question)
+                            }
+                            ORDER BY asc(?awardCount)
                             "
                    },
                    new Question
@@ -4102,7 +4106,7 @@ namespace WikidataGame.Backend.Helpers
                        GroupId = new Guid("0b1ff760-e02f-4ddc-8f32-5161931ebcbe"),
                        MiniGameType = MiniGameType.Sort,
                        Status = QuestionStatus.Approved,
-                       TaskDescription = "Sort these sports by participating players.",
+                       TaskDescription = "Sort these sports by maximum amount ofparticipating players.(ascending)",
                        SparqlQuery = @"
                             SELECT (Sample(GROUP_CONCAT( DISTINCT ?question; SEPARATOR=', ')) AS ?question) (SAMPLE(GROUP_CONCAT(DISTINCT SAMPLE(?answer); SEPARATOR=', ')) AS ?answer) 
                             ?playerCount
